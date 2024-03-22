7dac5671146f2061ed23461833b141b96ab9140c
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2024-03-22 08:32:11 +0100

    ci: add worklows and verify

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## [Unreleased]
+
+# 
+
+
+[Unreleased]: https://github.com/Qwitqwit/qwit-desktop/compare/v1.5.0...HEAD
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      white: "#ffffff",
+      black: "#000000",
+      primary: "#e59883",
+      "dark-accent": "#83005d",
+      accent: "#e583c3",
+      neutral: "#83005d",
+    },
+  },
+  plugins: [],
+};
